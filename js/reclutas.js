0eed56061abaf2d5eb67a9daaba84b514fc56c86
Esteban Gonzalez <128927257+ESTEB4NGONZ4LEZ@users.noreply.github.com>
2023-05-09 13:52:22 -0500

    Proyecto Terminado

--- js/reclutas.js
+++ js/reclutas.js
@@ -8,7 +8,7 @@ const tableReclutas = document.querySelector(".table-reclutas");
 
 btnRecultas.addEventListener("click", (e) => {
     e.preventDefault();
-    e.stopImmediatePropagation()
+    e.stopImmediatePropagation();
     formReclutas.style.display = "block";
     tableReclutas.style.display = "none";
 });
@@ -45,10 +45,12 @@ function imprimirReclutas() {
             <td>${data.idTeam}</td>
             <td>
                 <button id="eliminarRecluta" class="btn btn-danger" data-id="${data.id}">ELIMINAR</button>
+                <button id="editar" class="btn btn-warning" onclick="datosCampos(${data.id})" data-idput="${data.id}">EDITAR</button>
             </td>
         </tr>
         `;
         listenerEliminarRecluta();
+        obtenerIdRecluta();
     });
 }
 /* FIN IMPRIMIR RECLUTAS */
@@ -84,6 +86,80 @@ btnGuardar.addEventListener("click", (e) => {
     });
 });
 /* FIN POST */
+
+/* INICIO PUT */
+function datosCampos(reclutaId) {
+    let recluta = reclutas.filter((e) => {
+        return e.id == reclutaId;
+    })[0];
+
+    document.querySelector("#nombre").value = recluta.nombre;
+    document.querySelector("#edad").value = recluta.edad;
+    document.querySelector("#telefono").value = recluta.telefono;
+    document.querySelector("#email").value = recluta.email;
+    document.querySelector("#direccion").value = recluta.direccion;
+    document.querySelector("#fechaNacimiento").value = recluta.fechaNacimiento;
+    document.querySelector("#numeroIdentificacion").value =
+        recluta.numeroIdentificacion;
+    document.querySelector("#fechaIngreso").value = recluta.fechaIngreso;
+    document.querySelector("#idTeam").value = recluta.idTeam;
+    document.querySelector("#reclutaId").value = recluta.id;
+
+    const btnGuardar = document.querySelector("#guardarRecluta");
+    const btnActualizar = document.querySelector("#actualizarRecluta");
+
+    formReclutas.style.display = "block";
+    btnActualizar.style.display = "block";
+    btnGuardar.style.display = "none";
+}
+
+obtenerIdRecluta = () => {
+    const btnEditar = document.querySelectorAll("#editar");
+
+    btnEditar.forEach((element) => {
+        element.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            actualizarRecluta(e.target.dataset.idput);
+        });
+    });
+};
+
+function actualizarRecluta(id) {
+    const btnActualizar = document.querySelector("#actualizarRecluta");
+    btnActualizar.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        let data = {
+            nombre: document.querySelector("#nombre").value,
+            edad: document.querySelector("#edad").value,
+            telefono: document.querySelector("#telefono").value,
+            email: document.querySelector("#email").value,
+            direccion: document.querySelector("#direccion").value,
+            fechaNacimiento: document.querySelector("#fechaNacimiento").value,
+            numeroIdentificacion: document.querySelector(
+                "#numeroIdentificacion"
+            ).value,
+            fechaIngreso: document.querySelector("#fechaIngreso").value,
+            idTeam: document.querySelector("#idTeam").value,
+            id: document.querySelector("#reclutaId").value,
+        };
+
+        fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((repuesta) => {
+            getReclutas();
+        });
+    });
+}
+
+/* FIN PUT */
 
 /* INICIO DELETE */
 listenerEliminarRecluta = () => {
